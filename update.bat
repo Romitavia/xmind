0b88146e16abc773a670e42e8ef6f7f560517c9a
your name <your email>
2020-04-29 02:22:50 +0800

    XMIND

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo on
 @title bat execute git auto commit
-cd D:/xmind
+cd D:\xmind
 git add .
 git commit -m "XMIND"
-git config --global credential.helper store
-git push -u origin master
+git push origin master
+@pause
