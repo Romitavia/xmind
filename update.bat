c81f9ba1c7b4e42c3fdc1574b1fc3062356cef3c
your name <your email>
2020-04-29 02:25:54 +0800

    XMIND

--- update.bat
+++ update.bat
@@ -1,7 +1,10 @@
 @echo on
 @title bat execute git auto commit
 cd D:\xmind
+git config --global user.email "645195574@qq.com"
+git config --global user.name "Romitavia"
 git add .
 git commit -m "XMIND"
+git pull
 git push origin master
 @pause
