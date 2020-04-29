fa310810948c7db2c501c895756445e892077a5c
your name <your email>
2020-04-29 23:14:29 +0800

    XMIND

--- update.bat
+++ update.bat
@@ -1,8 +1,6 @@
-@echo no
+@echo This is auto-upload/download github
 @title bat execute git auto commit
 cd /d D:\xmind\
-git config --global user.email "645195574@qq.com"
-git config --global user.name "Romitavia"
 git add .
 git commit -m "XMIND"
 git pull
