cbe08d081388b3b49347dffbf1a5faefd0e87203
your name <your email>
2020-07-20 19:00:02 +0800

    XMIND

--- update.bat
+++ update.bat
@@ -1,6 +1,8 @@
 @echo This is auto-upload/download github
 @title bat execute git auto commit
 cd /d D:\xmind\
+git remote rm origin
+git remote add origin https://github.com/Romitavia/xmind.git
 git add .
 git commit -m "XMIND"
 git pull
